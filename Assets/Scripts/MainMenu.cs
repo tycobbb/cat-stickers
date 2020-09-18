59fb3f61ea0edbf649592a18f06adf4f337cb7c2
Ty Cobb <ty.cobb.m@gmail.com>
2020-09-18 17:07:37 -0400

    add chatter dialog, fix some of the pacing

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu: Fungus.MenuDialog {
+    // -- commands --
+    public override bool AddOption(string text, bool interactable, bool hideOption, Fungus.Block targetBlock) {
+        // add the option
+        var result = base.AddOption(text, interactable, hideOption, targetBlock);
+
+        // show the appropriate dividers
+        var i = 0;
+        var dividers = gameObject.GetComponentsInChildren<Image>(true);
+
+        foreach (var divider in dividers) {
+            if (divider.name != "Divider") {
+                continue;
+            }
+
+            // show any dividers between two active buttons (i.e. n-1 dividers) and
+            // hide the rest
+            var isVisible = i < DisplayedOptionsCount - 1;
+            divider.gameObject.SetActive(isVisible);
+            i++;
+        }
+
+        return result;
+    }
+}
