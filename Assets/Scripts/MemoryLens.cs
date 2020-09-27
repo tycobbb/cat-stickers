ce819a14619dec1bcde5ab968506494a232d6840
Ty Cobb <ty.cobb.m@gmail.com>
2020-09-27 10:30:22 -0400

    unpolished version of memory frame advance

---
+++ Assets/Scripts/MemoryLens.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryLens: MonoBehaviour {
+    // -- fields --
+    [Tooltip("The actual menu")]
+    [SerializeField]
+    protected SpriteRenderer fMemory;
+
+    // -- commands --
+    public void SetMemory(SpriteRenderer memory) {
+        fMemory = memory;
+        SetPercentComplete(0.0f);
+    }
+
+    public void SetPercentComplete(float percent) {
+        fMemory.color = new Color(1, 1, 1, percent);
+    }
+}
---
+++ Assets/Scripts/ShowMemory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using F = Fungus;
+using UI = UnityEngine.UI;
+
+[F.CommandInfo("Custom", "ShowMemory", "Set alpha of the current memory.")]
+[AddComponentMenu("")]
+public class ShowMemory: F.Command {
+    // -- fields --
+    [Tooltip("The step variable")]
+    [SerializeField]
+    protected F.IntegerData fStep;
+
+    [Tooltip("The last step variable")]
+    [SerializeField]
+    protected F.IntegerData fLastStep;
+
+    [Tooltip("The memory container")]
+    [SerializeField]
+    protected MemoryLens fLens;
+
+    // -- lifecycle --
+    public override void OnEnter() {
+        var percent = (float)(fStep.Value + 1) / fLastStep.Value;
+        fLens.SetPercentComplete(percent);
+        Continue();
+    }
+}
